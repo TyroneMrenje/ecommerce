c8ae28e96d8584bc4ecb4011634b2780ec8d60ff
Tyrone Mrenje <tyronemrenje1985@gmail.com>
2026-07-06 00:37:02 +0300

    update

--- resources/js/pages/login.tsx
+++ resources/js/pages/login.tsx
@@ -1,42 +1,63 @@
 import React from "react";
-import {Form,Head } from "@inertiajs/react";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import {Form,Head, Link } from "@inertiajs/react";
 import { useState } from "react";
 import { LuEyeClosed, LuEye } from "react-icons/lu";
 
 export default function Login(){
+     
+    const[showPassword, setShowPassword] =useState(false);
 
     return(
-        <div>
+        <div className="box-border overflow-hidden scroll-smooth">
             <Head title="Login"/>
-            <Navbar/>
-            <Form method="POST" action='/users/login'
-            resetOnSuccess
-            disableWhileProcessing
-            className="flex flex-col justify-center items-center h-screen"
-            >
-           {({
-              errors,
-              validate,
-              valid,
-              invalid,
-              processing
-           })=>(
-               <>
-                 <label htmlFor="email">Enter email</label>
-                 <input required type="email" name="email" onChange={()=>validate('email')}/>
-                {valid('email') && <p className="text-sm text-green-500">valid</p>}
-                {invalid('email') && <p className="text-sm text-red-500">{errors.email}</p>}
-                <label htmlFor="password">Enter password</label>
-                <input required type="password" name="password" onChange={()=>validate('password')}/>
-                {invalid('password') && <p className="text-sm text-red-500">{errors.password}</p>}
-                <button type="submit" disabled={processing}>Login</button>
-
-               </>
-
-           )}
+            <div className="flex flex-col items-center justify-center h-screen">
+                 <div className="relative bottom-20">
+                    <Link href="/" prefetch>        
+                      <h1 className="font-bold text-[#a2252a] text-3xl hover:pointer">Amimo Spices</h1>  
+                    </Link>
+                </div>    
+                <Form method="POST" action='/users/login'
+                    resetOnSuccess
+                    disableWhileProcessing
+                    className="flex flex-col justify-center items-start space-y-4"
+                    >
+                {({
+                    errors,
+                    validate,
+                
+                    invalid,
+                    processing
+                })=>(
+                    <>
+                         <div>
+                            <p className="text-2xl font-bold">Sign up</p>
+                            <h1 className="text-sm text-gray-500 font-normal">Sign up or create an account</h1>              
+                         </div>
+                        <input required type="email" name="email" className="border border-gray-400 p-3 w-80 rounded-lg"  placeholder="Enter email" onChange={()=>validate('email')}/>
+                         {invalid('email') && <p className="text-sm text-red-500">{errors.email}</p>}
+                      
+                        <div className="flex flex-row items-center">
+                            <input required type={showPassword ? 'text' : 'password'} name="password" className="border border-gray-400 p-3 w-80 rounded-lg" onChange={()=>validate('password')} placeholder="Enter password"/>
+                          <button
+                            type="button" 
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="relative right-10 text-gray-400 hover:text-gray-600"
+                            >
+                            {showPassword ? <LuEyeClosed className="w-5 h-6"/> : <LuEye className="w-5 h-6"/>}
+                         </button>
+                        {invalid('password') && <p className="text-sm text-red-500">{errors.password}</p>}
+                
+                        </div>
+                         <div className="flex flex-col w-full items-center space-y-2">
+                            <button type="submit" disabled={processing} className="bg-[#a2252a] rounded-lg p-2 w-60 text-white transition delay-80 duration-200 ease-in-out  hover:scale-110 ">Login</button>
+                            <Link href="/user/register" className="hover:underline hover:text-blue-500" prefetch>
+                                <button className="hover-underline">Don't have an account,log in</button>
+                            </Link>
+                        </div>
+                    </>
+               )}
             </Form>
+            </div>               
         </div>
     )
 }
--- resources/js/pages/register.tsx
+++ resources/js/pages/register.tsx
@@ -10,12 +10,17 @@ export default function Register(){
 
    return(
      <div className="box-border overflow-hidden scroll-smooth">
-        <Head title="Register"/>        
-        <div className="">
+        <Head title="Register"/>     
+        <div className="flex flex-col items-center justify-center h-screen">         
+           <div className="relative bottom-20">
+             <Link href="/" prefetch>
+                <h1 className="font-bold text-[#a2252a] text-3xl">Amimo Spices</h1>  
+             </Link>
+            </div>    
             <Form method="POST" action="/user/register" 
             resetOnSuccess
             disableWhileProcessing
-            className="flex flex-col justify-center items-center h-screen"
+            className="flex flex-col justify-center items-start space-y-4"
             >
             {({
                 errors,
@@ -25,41 +30,47 @@ export default function Register(){
                 processing
             })=>(
                 <>
-                <label htmlFor="name"> Enter name</label>
-                <input required type="text" name="name" className="border-2" onChange={()=>validate('name')}/>
+                <div>
+                  <p className="text-2xl font-bold">Sign up</p>
+                  <h1 className="text-sm text-gray-500 font-normal">Sign up or create an account</h1>              
+                </div>
+                <input required type="text" name="name" placeholder="Enter name" className="border border-gray-400 p-3 w-80 rounded-lg" onChange={()=>validate('name')}/>
                 {invalid('name') && <p className="text-sm text-error">{errors.name}</p>}
                 
-                <label htmlFor="email">Enter email</label>
-                <input  required type="email" name="email" onChange={()=>validate('email')}/>
+                <input  required type="email" name="email" placeholder="Enter email" onChange={()=>validate('email')} className="border border-gray-400 p-3 w-80 rounded-lg"/>
                 {invalid('email') && <p className="text-sm text-error">{errors.email}</p>}
                 
-                <label htmlFor="password">Enter password</label>
-                <input  required type={showPassword? 'text': 'password'} name="password" onChange={()=>validate('password')}/>
-                <button
+               <div className="flex flex-row items-center">
+                 <input  required type={showPassword? 'text': 'password'} name="password" placeholder="Enter password" onChange={()=>validate('password')} className="border border-gray-400 p-3 w-80 rounded-lg"/>
+                 <button
                    type="button" 
                    onClick={() => setShowPassword(!showPassword)}
-                   className=" right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
-                 >
+                   className="relative right-10 text-gray-400 hover:text-gray-600"
+                  >
                     {showPassword ? <LuEyeClosed/> : <LuEye/>}
                  </button>
                 {invalid('password') && <p className="text-red-500">{errors.password}</p>}
+               </div>
 
-                <label htmlFor="password_confirmation">Enter password</label>
-                <input required type={showPassword? 'text': 'password'} name="password_confirmation" onChange={()=>validate('password_confirmation')}/>
-                <button
+               <div className="flex flex-row items-center">
+                 <input required type={showConfirmPassword? 'text': 'password'} name="password_confirmation" placeholder="Confirm Password" onChange={()=>validate('password_confirmation')} className="border border-gray-400 p-3 w-80 rounded-lg"/>
+                 <button
                    type="button" 
                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                   className=" right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
+                   className=" relative right-10 text-gray-400 hover:text-gray-600"
                  >
                     {showConfirmPassword ? <LuEyeClosed/> : <LuEye/>}
                  </button>
         
                 {invalid('password_confirmation') && <p className="text-error">{errors.password}</p>}
 
-                <button type="submit" disabled={processing}>Register</button>
+               </div>
+               <div className="flex flex-col w-full items-center space-y-2">
+                <button type="submit" disabled={processing} className="bg-[#a2252a] rounded-lg p-2 w-60 text-white transition delay-80 duration-200 ease-in-out  hover:scale-110 ">Register</button>
                 <Link href="/user/login" className="hover:underline hover:text-blue-500" prefetch>
                   <button className="hover-underline">Already have an account,log in</button>
                 </Link>
+               </div>
                 </>  
             )}
             </Form>
